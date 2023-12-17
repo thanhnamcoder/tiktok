78883a9ed70aed749f0dac525024c2eb37f5d137
thanhnam <nguyenthanhnamnv996@gmail.com>
2023-12-17 17:12:59 +0700

    update code

--- Oauth2.py
+++ Oauth2.py
@@ -1,5 +1,9 @@
 from boxsdk import Client, OAuth2
 import os
+import requests
+from datetime import datetime
+import pytz
+
 
 saved_access_token = None
 saved_refresh_token = None
@@ -53,3 +57,158 @@ def oauth2_process():
         store_tokens_to_file(access_token, refresh_token)
 
     return oauth
+
+
+
+
+
+
+
+
+
+
+
+def download_file_by_name(client, folder_id, file_name, output_directory):
+    if not os.path.exists(output_directory):
+        os.makedirs(output_directory)
+        print(f"Created directory: {output_directory}")
+
+    items = client.folder(folder_id).get_items()
+
+    for item in items:
+        if item.name == file_name:
+            try:
+                output_path = os.path.join(output_directory, item.name)
+                with open(output_path, 'wb') as file_output:
+                    file_content = client.file(file_id=item.id).content()
+                    file_output.write(file_content)
+                # print(f"Downloaded {file_name} successfully to {output_directory}")
+                return True
+            except Exception as e:
+                print(f"Error downloading {file_name}: {e}")
+                return False
+    print(f"File {file_name} not found in Box.")
+    return False
+
+def delete_file_by_name(client, folder_id, file_name):
+    items = client.folder(folder_id).get_items()
+    
+    for item in items:
+        if item.name == file_name:
+            try:
+                client.file(file_id=item.id).delete()
+                # print(f"Deleted {file_name} successfully")
+                return True
+            except Exception as e:
+                print(f"Error deleting {file_name}: {e}")
+                return False
+
+def get_single_tiktok_info(file_path):
+    url = ''
+    description = ''
+
+    try:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            lines = file.readlines()
+
+            for line in lines:
+                line = line.strip()
+                if line.startswith('URL TikTok:'):
+                    url = line.replace('URL TikTok: ', '')
+                elif line.startswith('Description:'):
+                    description = line.replace('Description: ', '')
+                    break  # Dừng sau khi đã tìm thấy một cặp URL và mô tả
+
+    except Exception as e:
+        print(f"An error occurred while reading the file: {str(e)}")
+    
+    return url, description
+
+
+def delete(ten_file):
+    try:
+        deleted_content = []  # Initialize a list to store deleted content
+        with open(ten_file, 'r', encoding='utf-8') as file:
+            lines = file.readlines()
+
+            # Store the content to be deleted
+            deleted_content = lines[:3]
+
+        with open(ten_file, 'w', encoding='utf-8') as file:
+            for index, line in enumerate(lines):
+                if index < 3:
+                    continue
+                file.write(line)
+
+        # Construct URL and description of deleted content
+        deleted_description = ''.join(deleted_content)
+        return f"\nĐã xóa:\n{deleted_description}"
+    
+    except FileNotFoundError:
+        return "File không tồn tại"
+    except Exception as e:
+        return f"Lỗi: {str(e)}"
+    
+def send_message(webhook_url, file_path, description):
+
+    # Mở file để đọc dữ liệu
+    with open(file_path, 'rb') as file:
+        file_data = file.read()
+    
+    # Tạo nội dung tin nhắn
+    message = f'{description}'
+    
+    # Tạo payload để gửi
+    payload = {'content': message}
+    
+    
+    # Gửi yêu cầu POST với multipart/form-data
+    response = requests.post(webhook_url, data=payload)
+    
+    return response.text  
+    
+def send_message_with_file(webhook_url, file_path, url_tikok, date_time):
+
+    # Mở file để đọc dữ liệu
+    with open(file_path, 'rb') as file:
+        file_data = file.read()
+    
+    # Tạo nội dung tin nhắn
+    message = f'{date_time}\n{url_tikok}'
+    
+    # Tạo payload để gửi
+    payload = {'content': message}
+    
+    # Thêm file vào payload với key là 'file' và giá trị là dữ liệu của file
+    files = {'file': (file_path, file_data)}
+    
+    # Gửi yêu cầu POST với multipart/form-data
+    response = requests.post(webhook_url, data=payload, files=files)
+    
+    return response.text
+
+def delete_file_in_folder(file_path):
+    try:
+        if os.path.exists(file_path):
+            os.remove(file_path)
+            # print(f"Đã xóa tệp {file_path} thành công")
+        else:
+            print(f"Tệp {file_path} không tồn tại")
+    except Exception as e:
+        print(f"Lỗi: {str(e)} khi xóa tệp {file_path}")
+
+
+
+def current_time_with_format():
+    # Lấy thời gian hiện tại ở múi giờ UTC
+    now_utc = datetime.utcnow()
+
+    # Chuyển đổi múi giờ từ UTC sang múi giờ Việt Nam (UTC+7)
+    vn_tz = pytz.timezone('Asia/Ho_Chi_Minh')
+    time_vietnam = pytz.utc.localize(now_utc).astimezone(vn_tz)
+
+    # Định dạng thời gian với năm đầy đủ (yyyy)
+    formatted_time = time_vietnam.strftime("Time: %H:%M:%S\nDate: %d/%m/%Y ")
+
+    # Trả về thời gian hiện tại ở múi giờ Việt Nam
+    return formatted_time
--- send_content_to_discord.py
+++ send_content_to_discord.py
@@ -1,157 +1,23 @@
-import re
-import requests
+import discord
+from discord.ext import tasks
 from Oauth2 import *
-import time
-from datetime import datetime
-import pytz
+import re
 
 input_folder_name = "_dun.hen16"
 file_path = f"videos/description/{input_folder_name}_description.txt"
-
-def current_time_with_format():
-    # Lấy thời gian hiện tại ở múi giờ UTC
-    now_utc = datetime.utcnow()
-
-    # Chuyển đổi múi giờ từ UTC sang múi giờ Việt Nam (UTC+7)
-    vn_tz = pytz.timezone('Asia/Ho_Chi_Minh')
-    time_vietnam = pytz.utc.localize(now_utc).astimezone(vn_tz)
-
-    # Định dạng thời gian với năm đầy đủ (yyyy)
-    formatted_time = time_vietnam.strftime("Time: %H:%M:%S\nDate: %d/%m/%Y ")
-
-    # Trả về thời gian hiện tại ở múi giờ Việt Nam
-    return formatted_time
-
-def download_file_by_name(client, folder_id, file_name, output_directory):
-    if not os.path.exists(output_directory):
-        os.makedirs(output_directory)
-        print(f"Created directory: {output_directory}")
-
-    items = client.folder(folder_id).get_items()
-
-    for item in items:
-        if item.name == file_name:
-            try:
-                output_path = os.path.join(output_directory, item.name)
-                with open(output_path, 'wb') as file_output:
-                    file_content = client.file(file_id=item.id).content()
-                    file_output.write(file_content)
-                print(f"Downloaded {file_name} successfully to {output_directory}")
-                return True
-            except Exception as e:
-                print(f"Error downloading {file_name}: {e}")
-                return False
-    print(f"File {file_name} not found in Box.")
-    return False
-
-def delete_file_by_name(client, folder_id, file_name):
-    items = client.folder(folder_id).get_items()
-    
-    for item in items:
-        if item.name == file_name:
-            try:
-                client.file(file_id=item.id).delete()
-                print(f"Deleted {file_name} successfully")
-                return True
-            except Exception as e:
-                print(f"Error deleting {file_name}: {e}")
-                return False
-
-def get_single_tiktok_info(file_path):
-    url = ''
-    description = ''
-
-    try:
-        with open(file_path, 'r', encoding='utf-8') as file:
-            lines = file.readlines()
-
-            for line in lines:
-                line = line.strip()
-                if line.startswith('URL TikTok:'):
-                    url = line.replace('URL TikTok: ', '')
-                elif line.startswith('Description:'):
-                    description = line.replace('Description: ', '')
-                    break  # Dừng sau khi đã tìm thấy một cặp URL và mô tả
-
-    except Exception as e:
-        print(f"An error occurred while reading the file: {str(e)}")
-    
-    return url, description
+box_folder_id = "0"
+folder_video_path = "videos/videos"
+webhook_url = 'https://discordapp.com/api/webhooks/1185876707043983493/kEyLEsJPHzyEyplAk3Z9ikEfqRO5N2HwaPE0fiBNqiWArwpNlk81LwGYbno22lF3dYVx'
 
 
-def delete(ten_file):
-    try:
-        deleted_content = []  # Initialize a list to store deleted content
-        with open(ten_file, 'r', encoding='utf-8') as file:
-            lines = file.readlines()
+intents = discord.Intents.default()
+intents.messages = True
 
-            # Store the content to be deleted
-            deleted_content = lines[:3]
+client = discord.Client(intents=intents)
 
-        with open(ten_file, 'w', encoding='utf-8') as file:
-            for index, line in enumerate(lines):
-                if index < 3:
-                    continue
-                file.write(line)
-
-        # Construct URL and description of deleted content
-        deleted_description = ''.join(deleted_content)
-        return f"\nĐã xóa:\n{deleted_description}"
-    
-    except FileNotFoundError:
-        return "File không tồn tại"
-    except Exception as e:
-        return f"Lỗi: {str(e)}"
-    
-def send_message(webhook_url, file_path, description):
-
-    # Mở file để đọc dữ liệu
-    with open(file_path, 'rb') as file:
-        file_data = file.read()
-    
-    # Tạo nội dung tin nhắn
-    message = f'{description}'
-    
-    # Tạo payload để gửi
-    payload = {'content': message}
-    
-    
-    # Gửi yêu cầu POST với multipart/form-data
-    response = requests.post(webhook_url, data=payload)
-    
-    return response.text  
-    
-def send_message_with_file(webhook_url, file_path, url_tikok, date_time):
-
-    # Mở file để đọc dữ liệu
-    with open(file_path, 'rb') as file:
-        file_data = file.read()
-    
-    # Tạo nội dung tin nhắn
-    message = f'{date_time}\n{url_tikok}'
-    
-    # Tạo payload để gửi
-    payload = {'content': message}
-    
-    # Thêm file vào payload với key là 'file' và giá trị là dữ liệu của file
-    files = {'file': (file_path, file_data)}
-    
-    # Gửi yêu cầu POST với multipart/form-data
-    response = requests.post(webhook_url, data=payload, files=files)
-    
-    return response.text
-
-def delete_file_in_folder(file_path):
-    try:
-        if os.path.exists(file_path):
-            os.remove(file_path)
-            print(f"Đã xóa tệp {file_path} thành công")
-        else:
-            print(f"Tệp {file_path} không tồn tại")
-    except Exception as e:
-        print(f"Lỗi: {str(e)} khi xóa tệp {file_path}")
-
-def main():
+@client.event
+async def on_ready():
+    print('Bot đã kết nối thành công!')
     oauth = oauth2_process()
     client = Client(oauth)
     webhook_url = 'https://discordapp.com/api/webhooks/1185107643145134090/5HgKD6JC_fpKRdFuHWOmESzcn4t7GRvbnEuTfqef4mQqJjEdj77xjncMPmy8ssiW-tiW'
@@ -164,7 +30,43 @@ def main():
     download_file_by_name(client, box_folder_id, file_descreption_to_download, folder_description_path)
     delete_file_by_name(client, box_folder_id, file_descreption_to_download)
 
-    while True:
+
+    date_time = current_time_with_format()
+    oauth = oauth2_process()
+    client = Client(oauth)
+    tiktok_url, tiktok_description = get_single_tiktok_info(file_path)
+    tiktok_id = re.sub(r'.*?/(\d+)$', r'\1_effect', tiktok_url)
+    file_name_to_download = f"{tiktok_id}.mp4"
+    download_success = download_file_by_name(client, box_folder_id, file_name_to_download, folder_video_path)
+    if download_success:
+        print("Tải video thành công!")
+    else:
+        print("Không thể tải video.")
+    delete_file_by_name(client, box_folder_id, file_name_to_download)
+    delete(file_path)
+    file_path_up_to_discord = f"videos/videos/{file_name_to_download}"
+    # print(file_path_up_to_discord)
+    send_message(webhook_url, file_path_up_to_discord, tiktok_description)
+    response_text = send_message_with_file(webhook_url, file_path_up_to_discord, tiktok_url, date_time)
+    # print(response_text)
+    delete_file_in_folder(file_path_up_to_discord)
+    print("Gửi tin nhắn thành công")
+
+
+@client.event
+async def on_message(message):
+    if message.reference: 
+        original_msg_id = message.reference.message_id
+        original_msg = await message.channel.fetch_message(original_msg_id)
+        await original_msg.delete()
+        await message.delete()  # This deletes the replied message
+        # await message.channel.send(f"ID tin nhắn gốc bạn đã reply là: {original_msg_id}") 
+        
+        
+
+
+@tasks.loop(seconds=60)
+async def send_loop_message():
         date_time = current_time_with_format()
         oauth = oauth2_process()
         client = Client(oauth)
@@ -180,14 +82,18 @@ def main():
         delete_file_by_name(client, box_folder_id, file_name_to_download)
         delete(file_path)
         file_path_up_to_discord = f"videos/videos/{file_name_to_download}"
-        print(file_path_up_to_discord)
+        # print(file_path_up_to_discord)
         send_message(webhook_url, file_path_up_to_discord, tiktok_description)
         response_text = send_message_with_file(webhook_url, file_path_up_to_discord, tiktok_url, date_time)
-        print(response_text)
+        # print(response_text)
         delete_file_in_folder(file_path_up_to_discord)
+        print("Gửi tin nhắn thành công")
+    
+@client.event
+async def on_ready():
+    print('Bot đã kết nối thành công!')
+    # Start the loop here when the bot is ready
+    send_loop_message.start()
 
-        # Chờ 5 giây trước khi thực hiện lại
-        time.sleep(3600)
-        
-if __name__ == "__main__":
-    main()
+# Thay YOUR_BOT_TOKEN bằng token của bot Discord của bạn
+client.run('MTE4NTgyMzM5NTk1OTUzNzcxNg.Gbr0JR.roJG50T3hEu_ANXAYsewDLC8rrWDFoaeywN5IQ')
