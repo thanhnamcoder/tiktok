08091da0decc8c84fff164a9614ce1253055c9f6
thanhnam <nguyenthanhnamnv996@gmail.com>
2024-02-19 13:28:42 +0700

    fafds

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from flask import Flask, request, jsonify
-import requests
 import json
 from Oauth2 import *
 import re
@@ -13,7 +12,6 @@ app = Flask(__name__)
 def perform_action():
     # Nhận dữ liệu từ yêu cầu POST
     data = request.get_json()
-    
     # Khởi tạo biến result với giá trị mặc định
     result = "Hành động không được thực hiện vì không có dữ liệu"
 
@@ -32,26 +30,19 @@ def perform_action():
     # Trả về kết quả dưới dạng JSON
     return jsonify({'result': result})
 
-
-
-
 def send_discord_webhook(webhook_url):
     try:
         # Xóa toàn bộ tin nhắn trong kênh trước khi gửi tin nhắn mới
         delete_messages_in_channel()
-        
         # Xác thực OAuth và tạo client
         oauth = oauth2_process()
         client = Client(oauth)
-        
         # Lấy thông tin video từ MongoDB
         tiktok_url, tiktok_description = get_single_tiktok_info_from_mysql()
         tiktok_id = re.sub(r'.*?/(\d+)$', r'\1_effect', tiktok_url)
         file_name_to_download = f"{tiktok_id}.mp4"
-        
         # Thư mục lưu trữ video
-        folder_video_path = "videos/videos"
-        
+        folder_video_path = "videos"
         # Đường dẫn đến tệp tin video sau khi tải xuống
         file_path_up_to_discord = os.path.join(folder_video_path, file_name_to_download)
         # Tải xuống tệp tin video từ Box
@@ -64,7 +55,6 @@ def send_discord_webhook(webhook_url):
         if not os.path.exists(file_path_up_to_discord):
             print(f"Tệp tin video '{file_name_to_download}' không tồn tại trong thư mục '{folder_video_path}'.")
             return False
-        
         # Gửi tin nhắn với mô tả đến Discord
         payload_description = {"content": tiktok_description}
         response_description = requests.post(webhook_url, data=json.dumps(payload_description), headers={"Content-Type": "application/json"})
@@ -80,21 +70,14 @@ def send_discord_webhook(webhook_url):
             if response_file.status_code != 200:
                 print("Đã xảy ra lỗi khi gửi tin nhắn tệp tin. Mã lỗi:", response_file.status_code)
                 return False
-    
         clean()
-
         return True, file_name_to_download
     except Exception as e:
         print("Đã xảy ra lỗi khi gửi tin nhắn:", str(e))
         return False
 
-
-
-
-
-
-
 if __name__ == '__main__':
-    app.run()
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port="8000")
 
 #scp -r "D:\Project\tiktok" automation@52.249.223.231:/home/automation
